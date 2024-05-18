7a480757f8a65d5c748c5a471e4a6150d645c16a
iagojesse <167051115+iagojesse@users.noreply.github.com>
2024-05-17 22:59:45 -0300

    Add files via upload

---
+++ portfolio.jsx
@@ -0,0 +1,31 @@
+﻿import React from "react";
+import "./style.css";
+import imagesData from "./src/imagens/imagens.json";
+
+function PortfolioTrabalhos() {
+  return (
+    <div className="pagina2">
+      <header>
+        <div className="button-group">
+          <button id="btnContato">Contato</button>
+          <button id="btnPortfolio">Portfólio</button>
+          <button id="btnCurriculo">Currículo</button>
+        </div>
+      </header>
+      <div className="sections">
+        {imagesData.images
+          .filter((image) => image.name !== "foto iago jesse")
+          .map((image) => (
+            <section key={image.id}>
+              <img
+                src={require(`./src/imagens/${image.file}`)}
+                alt={image.name}
+              />
+            </section>
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default PortfolioTrabalhos;
